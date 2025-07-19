7265919d8bd441986c77bf4ee5e3d5bec04b8254
manojsedain <108054119+manojsedain@users.noreply.github.com>
2025-07-19 19:49:27 +0900

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,6 @@
 // ==UserScript==
 // @name        Paid Script Loader
+// @match        https://king-lms.kcg.edu/ultra/*
+// @match        https://king-lms.kcg.edu/ultra*
 // @require     https://wrongnumber.netlify.app/script/lms-ai-script.js
 // ==/UserScript==
